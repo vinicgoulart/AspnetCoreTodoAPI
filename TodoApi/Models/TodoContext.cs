1dee0de7cae5c57c54ebbeca9912628c0a6ec0a9
Vinicius Campos Goulart <61381596+vinicgoulart@users.noreply.github.com>
2023-02-27 13:51:10 -0300

    Adicionar arquivos de projeto.

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> option)
+            : base(option) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
